cdbbd6695ae5f1dddd6d4a9619f9ede8dc2b1786
Kirill <matytsyn7@gmail.com>
2024-01-24 21:32:11 +0300

    finished code, fixed sum bugs

--- main.py
+++ main.py
@@ -37,3 +37,36 @@ mainframe.grid(padx=5, pady=5)
 amlable = ttk.Label(mainframe, text = "Количество кликов \n (оставьте 0 для значения по улмолчанию)").grid(column=1, row =1, sticky=W)
 speedlable = ttk.Label(mainframe, text = "Интервал между кликами \n (в миллисекундах)").grid(column=1, row=2, sticky=W)
 amountEntry = ttk.Entry(mainframe, textvariable=amount, width=5).grid(column=2, row=1)
+speedEntry = ttk.Entry(mainframe, textvariable=speed, width=5).grid(column=2, row=2)
+startButton = ttk.Button(mainframe, text="Начать", width=10, command=StateIsT).grid(column=1, row=3, columnspan=2, sticky=W)
+stopButton = ttk.Button(mainframe, text="Стоп", width=10,command=StateIsF).grid(column=2, row=3, columnspan=2,sticky=E)
+
+mainframe.bind("<F1>", StateIsT())
+mainframe.bind("<F3>", StateIsF())
+
+time=0
+x=0
+
+while True:
+    time+=1
+    if state==True:
+        amount = int(amount.get())
+        speed = int(speed.get())
+        print(state)
+        if amount==0:
+            time.sleep(speed/1000)
+            mouse.mouse_event(2,0,0,0,0)
+            mouse.mouse_event(4,0,0,0,0)
+            x+=1
+            print("Кликнуто " +x + " раз")
+
+        elif x <amount and state == True:
+            time.sleep(speed / 1000)
+            mouse.mouse_event(2, 0, 0, 0, 0)
+            mouse.mouse_event(4, 0, 0, 0, 0)
+            x += 1
+            print("Кликнуто " + x + " раз")
+            if x == amount:
+                state = False
+
+    root.update()
