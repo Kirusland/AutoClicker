6695e481166f2e22b99dc385ccf38c4fa1b72f61
Kirill <matytsyn7@gmail.com>
2024-01-24 21:17:08 +0300

    First commit with bug on 6 and 17 line

---
+++ main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkinter import ttk
+import time
+import ctypes
+
+mouse = ctypes.windll.user
+state = False
+
+def StateIsF():
+    global state
+    global counter
+    if state == True:
+        x = 0
+        state = False
+
+def StateIsT():
+    if state == False:
+        x = 0
+        state = True
+
+    else:
+        state = True
+
+root = Tk()
+root.title("AutoClocker")
+
+amount = StringVar()
+speed = StringVar()
+
+amount.set(0)
+speed.set(100)
+mainframe = ttk.Frame(root)
+mainframe.grid(padx=5, pady=5)
+
+amlable = ttk.Label(mainframe, text = "Количество кликов \n (оставьте 0 для значения по улмолчанию)").grid(column=1, row =1, sticky=W)
+speedlable = ttk.label(mainframe, text = "Интервал между кликами \n (в миллисекундах)").grid(column=1, row=2, sticky=W)
+amountEntry = ttk.Entry(mainframe, textvariable=amount, width=5).grid(column=2, row=1)
